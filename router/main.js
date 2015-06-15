949297f9fc2233b0cb154ff53524f3531a7072ac
John Paulino <paulinoj@gmail.com>
2015-06-15 16:38:11 -0700

    Reorganized router a bit

--- router/main.js
+++ router/main.js
@@ -1,8 +1,27 @@
 module.exports=function(app)
 {
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
+
 app.get('/',function(req,res){
 res.render('index.html');
 });
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  if (req.body.text === "Password") {
+    result = "Right";
+  }
+  else
+  {
+    result = "Wrong";
+  }
+  res.end(result);
+});
+
+
 // app.get('/about',function(req,res){
 // res.render('about.html');
 // });
