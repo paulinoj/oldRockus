fdeb64ba507c1f285ce82dc92d7127e1f7b0f984
John Paulino <paulinoj@gmail.com>
2015-06-15 19:56:46 -0700

    Basic functionality done

--- router/main.js
+++ router/main.js
@@ -32,15 +32,16 @@ var bodyParser = require('body-parser');
 
 var classicalSongs = [
     {
-        title: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3',
-        realTitle: 'Dancing Queens',
+        title: 'https://s3-us-west-1.amazonaws.com/mvpproject/TheLookOfLove.mp3',
+        realTitle: 'The Look Of Love',
         artist: '',
         start: 30,
         volume: 0.4
     },
+
     {
-        title: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3',
-        realTitle: 'Born To Run',
+        title: 'https://s3-us-west-1.amazonaws.com/mvpproject/ABBA+-+Dancing+Queen.mp3',
+        realTitle: 'Dancing Queen',
         artist: '',
         start: 30,
         volume: 0.4
@@ -86,6 +87,28 @@ app.post('/musicchoice', function(req, res) {
   res.end(JSON.stringify(songResponse));
 
 });
+
+app.post('/guess', function(req, res) {
+
+  var guess = req.body.text;
+  console.log(musicList);
+  console.log(guess);
+  var guessIndex = musicList.indexOf(guess);
+  console.log(guessIndex);
+
+  if (guessIndex !== -1) {
+    response = musicList[guessIndex];
+    musicList.splice(guessIndex, 1);
+  }
+  else
+  {
+    response = "";
+  }
+
+  res.end(response);
+
+});
+
 
 app.post('/', function(req, res) {
   console.log(req.body);
